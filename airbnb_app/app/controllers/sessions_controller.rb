c485465bfa08eae1cf549d9fc7467ceba7c142eb
Basso Basso <taylorpbasso@gmail.com>
2015-12-15 12:19:59 -0500

    debugged some sessions errors, now you can see the login page

--- airbnb_app/app/controllers/sessions_controller.rb
+++ airbnb_app/app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
-class SessionController < ApplicationController
+class SessionsController < ApplicationController
 
   def new
   end
 
   def create
-    user = User.find_by{email: params[:email]}
+    user = User.find_by({email: params[:email]})
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url

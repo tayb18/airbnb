a45f379db49938a8094da48acef9d5e98fb7725d
isabel cusick <cusick.isabel@gmail.com>
2015-12-15 11:45:57 -0500

    changes to postgres

--- airbnb_app/db/schema.rb
+++ airbnb_app/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151215151512) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "listings", force: :cascade do |t|
     t.text     "photo_url"
     t.string   "address"

90d70ef8af03842bc61dcd30eb7afe820c2016a0
isabel cusick <cusick.isabel@gmail.com>
2015-12-15 12:17:28 -0500

    working on signup

--- airbnb_app/app/controllers/users_controller.rb
+++ airbnb_app/app/controllers/users_controller.rb
@@ -1,12 +1,20 @@
 class UsersController < ApplicationController
-	def index
-		@users = User.all
-	end
+	
 
 	def new
+		@user = User.new
 	end
 
 	def create
+		
+		@user = User.create({name: params[:name], 
+			bio: params[:bio], 
+			img_url: params[:img_url], 
+			email: params[:email], 
+			password: params[:password], 
+			password_confirmation: params[:password_confirmation]})
+
+		redirect_to listings_path 
 	end
 
 	def show
@@ -14,4 +22,9 @@ class UsersController < ApplicationController
 
 	def update
 	end
+
+	# private 
+	# def users_params
+	# 	params.require(:user).permit(:name, :bio, :img_url, :email, :password, :password_confirmation)
+	# end
 end

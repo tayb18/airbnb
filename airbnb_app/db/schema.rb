fb3cbc4836aae9adf709cccddb7427cc45418dc5
bennettzjames <bennettzjames@gmail.com>
2015-12-15 11:39:03 -0500

    adds email column, and beginnings of listings controller

--- airbnb_app/db/schema.rb
+++ airbnb_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215151512) do
+ActiveRecord::Schema.define(version: 20151215162058) do
 
   create_table "listings", force: :cascade do |t|
     t.text     "photo_url"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20151215151512) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "email"
   end
 
 end
